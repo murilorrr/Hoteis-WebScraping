7d74eead9e22049910504c2515bf6214df8964c1
Murilo Ribeiro <murilorsv14@gmail.com>
2022-01-01 02:45:53 -0300

    finish the first day meta search any weather in the world

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const puppeteer = require('puppeteer');
+const readline = require('readline-sync');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
+  const cidade = await readline.question('qual nome da cidade desejada: ');
+  const url = `https://www.google.com/search?q=weather+${cidade}`;
+  await page.goto(url);
+  const temperatura = await page.evaluate(() =>{
+    return document.getElementById('wob_tm').innerText;
+  });
+  console.log(`A temperatura da ${cidade} é`,temperatura);
 
   await browser.close();
 })();
